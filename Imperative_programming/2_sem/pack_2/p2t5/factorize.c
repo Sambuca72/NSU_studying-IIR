8de7cae6fc2a7d648e4c9dc78da6ce798f7cb999
PanzerFaustt <89134662+PanzerFaustt@users.noreply.github.com>
2025-03-10 13:29:05 +0700

    p2t5 completed

--- Imperative_programming/2_sem/pack_2/p2t5/factorize.c
+++ Imperative_programming/2_sem/pack_2/p2t5/factorize.c
@@ -1,39 +1,38 @@
 typedef struct Factors {
-    int k; 
-    int primes [32];
-    int powers [32];
-    } Factors;
+    int k;
+    int primes[32];
+    int powers[32];
+} Factors;
 
-void Factorize(int X, Factors *res){
-    res -> k = 0;
-    
-    if (X == 1) 
+void Factorize(int X, Factors *res) {
+    res->k = 0;
+    if (X == 1) {
         return;
-    
+    }
     int x = X;
     int prime = 2;
-
-    while (x>1&&prime*prime >= x){
+    
+    
+    while (x > 1) {
         int power = 0;
-        while(x % prime == 0){
+        while (x % prime == 0) {
             power++;
             x /= prime;
         }
-        
-        if (power > 0){
-            res -> primes[res -> k] = prime;
-            res -> powers[res -> k] = power;
-            res -> k++;
+        if (power > 0) {
+            res->primes[res->k] = prime;
+            res->powers[res->k] = power;
+            res->k++;
         }
-
-        prime++;
-        if (prime == 4) prime = 5; 
-        else if (prime > 3) prime += 2;
-    }
-
-    if (x > 1) {
-        res->primes[res->k] = x;
-        res->powers[res->k] = 1;
-        res->k++;
+        
+        if (prime == 2) prime = 3;
+        else prime += 2;
+        
+        if (prime * prime > X && x > 1) {
+            res->primes[res->k] = x;
+            res->powers[res->k] = 1;
+            res->k++;
+            break;
+        }
     }
 }
